73fc7a6dd49610b014dfa7cdddf8584bc7fd77af
MarcosTag <marcossocimp5@gmail.com>
2025-04-03 22:22:02 -0300

    criada a função edit_entry da rota update_entry no AppController

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -34,6 +34,11 @@ class AppController extends Action {
         header( 'location: /' );
 	}
 
+    /**
+     * 
+     * 
+     * atualiza o estado do lançamento como efetivado ou não via ajax
+     */
     public function controller_update_entry_effected() {
 
         /** recebe um valor via post do ajax */
@@ -49,6 +54,36 @@ class AppController extends Action {
          */
         // $codificado = $_POST;
         // file_put_contents('novo.json', $codificado);
+
+    }
+
+    /**
+     * 
+     * 
+     * edita o lançamento no banco de dados
+     */
+    public function edit_entry() {
+
+        $data_post_filter = [
+            'entry_nature'              =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_nature'] ) ),
+            'entry_value'               =>  floatval( str_replace( ',', '.', $_POST['edit_entry_value'] ) ),
+            'entry_expected_date'       =>  preg_replace( '/[^0-9|&#039;|^-]/', '', \htmlspecialchars( $_POST['edit_entry_expected_date'] ) ),
+            'entry_type'                =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_type'] ) ),
+            'entry_description'         =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_description'] ) ),
+            'entry_recurrence'          =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_recurrence'] ) ),
+            'entry_qty_installments'    =>  preg_replace( '/[^1-9]/', '', \htmlspecialchars( $_POST['edit_entry_qty_installments'] ?? 1 ) ),
+            'entry_category'            =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_category'] ) ),
+            'entry_subcategory'         =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['edit_entry_subcategory'] ) ),
+        ];
+
+        $entryId = preg_replace( '/[^1-9]/', '', \htmlspecialchars( $_POST['entry_id'] ) );
+
+        $entry = Container::getModel( 'Entry' );
+
+        $entry->update_entry( $data_post_filter, $entryId );
+
+        header( 'location: /' );
+
 
     }
 
