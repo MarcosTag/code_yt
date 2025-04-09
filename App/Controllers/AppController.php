a93f9377138b42646587723cf31d1ee8b355703f
MarcosTag <marcossocimp5@gmail.com>
2025-04-09 16:44:04 -0300

    ajustada a variavel ['entry_qty_installments']

--- App/Controllers/AppController.php
+++ App/Controllers/AppController.php
@@ -22,7 +22,7 @@ class AppController extends Action {
             'entry_type'                =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['entry_type'] ) ),
             'entry_description'         =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['entry_description'] ) ),
             'entry_recurrence'          =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['entry_recurrence'] ) ),
-            'entry_qty_installments'    =>   preg_replace( '/[^1-9]/', '', \htmlspecialchars( $_POST['entry_qty_installments-installment'] > 1 ? $_POST['entry_qty_installments-installment'] : 1 ) ),
+            'entry_qty_installments'    =>   preg_replace( '/[^1-9]/', '', \htmlspecialchars( $_POST['entry_qty_installments'] > 1 ? $_POST['entry_qty_installments'] : 1 ) ),
             'entry_effected'            =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( ! isset( $_POST['entry_effected'] ) ? '0' : '1' ) ),
             'entry_category'            =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['entry_category'] ) ),
             'entry_subcategory'         =>  preg_replace( '[&lt;|&gt;|/|(alert)|(script)|(select)|(from)|(insert)|(into)|\(|\)|&#039;]', '', \htmlspecialchars( $_POST['entry_subcategory'] ) ),
@@ -32,6 +32,11 @@ class AppController extends Action {
         $entry->entry_save( $data_post_filter );
 
         header( 'location: /' );
+
+
+        // echo '<pre>';
+        // print_r( $_SERVER['REMOTE_PORT'] . $dateTime );
+        // echo '</pre>';
 
 	}
 
