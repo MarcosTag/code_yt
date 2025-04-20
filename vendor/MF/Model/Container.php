bd6e565d4479dbcb014d6012a02feab5d3b25772
MarcosTag <marcossocimp5@gmail.com>
2025-04-19 22:01:58 -0300

    foi criada mais uma função dentro da classe container, que instancia uma classe sem a necessidade de conectá-la com o banco de dados, pensado para conectar APIs;

--- vendor/MF/Model/Container.php
+++ vendor/MF/Model/Container.php
@@ -6,11 +6,18 @@ use App\Connection;
 
 class Container {
 
-	public static function getModel($model) {
-		$class = "\\App\\Models\\".ucfirst($model);
+	public static function getModel( $model ) {
+		$class = "\\App\\Models\\".ucfirst( $model );
 		$conn = Connection::getDb();
 
-		return new $class($conn);
+		return new $class( $conn );
+	}
+
+	public static function getModelNotDb( $model ) {
+
+		$class = "\\App\\Models\\".ucfirst( $model );
+		return new $class();
+
 	}
 }
 
